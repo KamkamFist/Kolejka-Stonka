f8ec6c76dcce262fadbeb57dbd89f0627071fb30
4313657 <4313657@TE-GDA-D-D26F20>
2024-09-27 11:37:31 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Herzlich Wilkommen! stonka");
+
+int klienci = 0;
+
+string[] imie = new string[5] {"Marek", "Anna", "Kajetan", "xd", "lol" };
+
+string[] kolejka = new string[5];
+
+
+
+
+while (true)
+{
+    var rand = new Random().Next(5);
+    Console.WriteLine("Przyszedł nowy klient! Co robisz (dodaj do kolejki[+]/kick[-])");
+
+    klienci++;
+
+    string decyzjaKlient = Console.ReadLine();
+
+    if (decyzjaKlient == "+")
+    {
+        kolejka[klienci] = imie[rand];
+        Console.WriteLine("Bierząca kolejka: ");
+        for (int i = 0; i < kolejka.Length; i++)
+        {
+            Console.WriteLine(kolejka[i]);
+        }
+
+       
+
+    }
+    else if (decyzjaKlient == "-")
+    {
+        Console.WriteLine("Klient zostal zgladzony");
+    }
+    else
+    {
+        Console.WriteLine("Nieprawidłowa decyzja!");
+    }
+}
